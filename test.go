ea25306dc36ea74227c3ea4cca5680784de443b2
wangjun <277372664@qq.com>
2019-01-02 19:14:11 +0800

    latest

---
+++ test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+	str :="alter table aa add index IN_a(`a`)"
+	reg :=regexp.MustCompile("^alter\\s+table")
+	data := reg.Find([]byte(str))
+	fmt.Println(string(data))
+}
