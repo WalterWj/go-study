cd984680dba0313f5c6c9b0133b7b1afb0530fc0
wangjun <277372664@qq.com>
2019-07-13 18:08:54 +0800

    Changes to be committed: 	modified:   bin/test 	modified:   src/myMath/myMath.go 	modified:   src/test.go
     Changes not staged for commit:
    	deleted:    .idea/codeStyles/codeStyleConfig.xml

--- src/myMath/myMath.go
+++ src/myMath/myMath.go
@@ -9,3 +9,14 @@ func Add(x, y int) int {
 func Sub(x, y int) int {
 	return x - y
 }
+
+func Max(x, y int) int {
+	var result int
+
+	if x > y {
+		result = x
+	} else {
+		result = y
+	}
+	return result
+}
--- src/test.go
+++ src/test.go
@@ -1,33 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"regexp"
-
-	mathClass "./myMath"
-)
+import "fmt"
 
 func main() {
-	/* A simple code */
-	fmt.Println("Hello, playground")
-	str := "alter table aa add index IN_a(`a`)"
-	reg := regexp.MustCompile("^alter\\s+table")
-	data := reg.Find([]byte(str))
-	fmt.Println(string(data))
-	fmt.Println(mathClass.Add(1, 2))
-	fmt.Println(mathClass.Sub(2, 1))
-	const (
-		a = 2 << iota
-		b
-	)
+	a, b := 100, 200
+	fmt.Printf("Begin A and B is: %d and %d \n", a, b)
 
-	fmt.Println(a, b)
+	swap(&a, &b)
+	fmt.Printf("Change A and B is: %d and %d \n", a, b)
+}
 
-	for m := 1; m < 10; m++ {
-		/*    fmt.Printf("第%d次：\n",m) */
-		for n := 1; n <= m; n++ {
-			fmt.Printf("%dx%d=%d ", n, m, m*n)
-		}
-		fmt.Println("")
-	}
+func swap(x *int, y *int) {
+	var temp int
+	temp = *x
+	*x = *y
+	*y = temp
 }
