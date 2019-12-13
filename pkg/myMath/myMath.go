74e99552287db88b642a85b0cc2fba08c50c9dd2
wangjun <277372664@qq.com>
2019-12-13 17:58:28 +0800

    add pkg

---
+++ pkg/myMath/myMath.go
@@ -0,0 +1,25 @@
+// myMath.go
+
+package mathClass
+
+import "fmt"
+
+func Add(x, y int) int {
+	return x + y
+	fmt.Println(x + y)
+}
+
+func Sub(x, y int) int {
+	return x - y
+}
+
+func Max(x, y int) int {
+	var result int
+
+	if x > y {
+		result = x
+	} else {
+		result = y
+	}
+	return result
+}
