5c9c3919bac376a16a6a8d50917230893bde4006
WalterWj <277372664@qq.com>
2020-07-03 17:52:31 +0800

    master

--- command/stats_dump.go
+++ command/stats_dump.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"path"
 
 	// import mysql
 	_ "github.com/go-sql-driver/mysql"
@@ -21,4 +23,34 @@ func Show(dsn string) {
 	}
 	fmt.Println(id)
 	defer db.Close()
+}
+
+// Write content
+func writeFile(dirname string, fileName string, content string, mode string) {
+	_, err := os.Stat(dirname)
+	if err != nil {
+		fmt.Printf("dir %s is not exist\n", dirname)
+		// mkdir dir
+		err := os.Mkdir(dirname, os.ModePerm)
+		if err != nil {
+			fmt.Printf("mkdir failed![%v]\n", err)
+		} else {
+			fmt.Printf("mkdir %s success!\n", dirname)
+		}
+	}
+
+	f, err := os.OpenFile(path.Join(dirname, fileName), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
+	if err != nil {
+		fmt.Println("os OpenFile error: ", err)
+		return
+	}
+	defer f.Close()
+	if mode == "nomal" {
+		f.WriteString(content + ";\n")
+	} else if mode == "json" {
+		f.WriteString(content + "\n")
+	} else {
+		content = "/*\n" + content + "\n*/"
+		f.WriteString(content + "\n")
+	}
 }
