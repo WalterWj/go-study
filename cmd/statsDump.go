3bda08aba1704f290898400cc6ee1efe8b72c61f
wangjun <277372664@qq.com>
2020-08-21 20:56:27 +0800

    master

--- cmd/statsDump.go
+++ cmd/statsDump.go
@@ -1,15 +1,18 @@
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
-
-	"github.com/WalterWj/go-study/command"
 	"github.com/spf13/cobra"
 )
 
 var (
-	dbhost, dbname, tbname, dbusername, dbpassword, id string
-	dbport, dbstatus                                   int
+	dbhost, dbname, tbname, dbusername, dbpassword string
+	dbport                                   int
+)
+
+const (
+	tables_q = "show tables"
 )
 
 // statsDumpCmd represents the statsDump command
@@ -21,25 +24,35 @@ var statsDumpCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("statsDump called")
 		dsn := fmt.Sprintf("%s:%s@(%s:%d)/%s", dbusername, dbpassword, dbhost, dbport, dbname)
-		command.Show(dsn)
+		db := mysqlConnect(dsn)
+		res := getTables(db)
+		fmt.Print(res,"\n")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(statsDumpCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// statsDumpCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// statsDumpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	statsDumpCmd.Flags().StringVarP(&dbusername, "dbusername", "u", "root", "Database user")
 	statsDumpCmd.Flags().StringVarP(&dbname, "dbname", "d", "test", "Database name")
 	statsDumpCmd.Flags().StringVarP(&dbhost, "dbhost", "H", "127.0.0.1", "Database host")
 	statsDumpCmd.Flags().StringVarP(&dbpassword, "dbpassword", "p", "", "Database passowrd")
 	statsDumpCmd.Flags().IntVarP(&dbport, "dbport", "P", 4000, "Database port")
+	statsDumpCmd.Flags().StringVarP(&tbname,"tbname", "t", "test", "Table name")
+}
+
+func getTables(db *sql.DB)  map[string]string{
+	var r = make(map[string]string)
+	rows, err := db.Query(tables_q)
+	ifErrWithLog(err)
+	defer rows.Close()
+
+	for rows.Next() {
+		var n, t string
+		err := rows.Scan(&t)
+		ifErrWithLog(err)
+		r[n] = t
+	}
+	err = rows.Err()
+	ifErrWithLog(err)
+	return r
 }
